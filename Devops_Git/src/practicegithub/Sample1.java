fb11eefbc771f57d64f41a3b6c323540fa647a1e
keerthinagareddy10 <ln555r@att.com>
2017-04-24 17:43:46 +0530

    Intial version checkin

---
+++ Devops_Git/src/practicegithub/Sample1.java
@@ -0,0 +1,14 @@
+package practicegithub;
+
+public class Sample1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Creating sample code for version 1");
+		System.out.println("Sample code version 1");
+	}
+
+}
